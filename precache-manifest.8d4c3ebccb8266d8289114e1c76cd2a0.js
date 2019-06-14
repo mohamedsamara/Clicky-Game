c90da0957d2046464170c3fdd96c94ffc8f44baf
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2019-06-13 20:47:27 -0700

    Updates

---
+++ precache-manifest.8d4c3ebccb8266d8289114e1c76cd2a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0453c8f0a1d0c0707235335fb1de7b7",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "f6ed4ae640979b766878",
+    "url": "/Clicky-Game/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9a40bba522c4a0574cb6",
+    "url": "/Clicky-Game/static/css/main.427266e2.chunk.css"
+  },
+  {
+    "revision": "f6ed4ae640979b766878",
+    "url": "/Clicky-Game/static/js/2.8cc29b78.chunk.js"
+  },
+  {
+    "revision": "9a40bba522c4a0574cb6",
+    "url": "/Clicky-Game/static/js/main.21fa2399.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
